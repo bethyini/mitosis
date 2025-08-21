85e255a1222a67a32e565bee93b02080d9a14651
luminescent13 <kaylee.l.ji@gmail.com>
2025-08-20 20:34:25 -0400

    initial cnn code (progress update)

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import yaml
 
 from utils import *
+from modeling import *
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 
 if __name__ == '__main__':
-    extract_labels(**config)
-    patch_labels(**config)
+    # PROCESSING DATA
+    # extract_labels(**config)
+    # patch_labels(**config)
+
+    # MODEL TRAINING
+    train_loader, val_loader, _ = get_dataloaders(**config)
+    model = CNNClassifer()
+    model = train(model, train_loader, val_loader, num_epochs=50, lr=5e-4)
---
+++ modeling/__init__.py
@@ -0,0 +1,3 @@
+from .train import train
+from .get_dataloaders import get_dataloaders
+from .models import CNNClassifer
